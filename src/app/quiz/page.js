0a0c360a7fc3a5342f6c08f9578b01f2fdb3dc83
5A12 <hirorogakuen@gmail.com>
2025-01-31 16:36:11 +0900

    Top画面でクイズ分岐、カスタマイズ性を考慮してクイズ分岐ページを設置(quiz/page.js)を利用

--- src/app/quiz/page.js
+++ src/app/quiz/page.js
@@ -1,6 +1,6 @@
-import React from "react";
-import QuizPage from "@/components/QuizPage"; // `@/components/` からインポート
+// import React from "react";
+// import QuizPage from "@/components/QuizPage"; // `@/components/` からインポート
 
-export default function QuizIndex() {
-  return <QuizPage />;
-}
+// export default function QuizIndex() {
+//   return <QuizPage />;
+// }
