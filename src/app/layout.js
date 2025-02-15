c73e782197719995f2bd8fa892b3d8b29652ad20
TAKE881 <hirorogakuen@gmail.com>
2025-02-15 16:09:24 +0900

    bgC2

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,7 +2,6 @@ import { Kaisei_Tokumin } from "next/font/google";
 import "./globals.css";
 
 const kaiseiTokumin = Kaisei_Tokumin({
-
   subsets: ["latin-ext"],
   weight: ["400", "700"],
 });
@@ -11,7 +10,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="ja">
       <body className={kaiseiTokumin.className}>
-        <div className="w-screen h-screen bg-[url('/gradation-612x612.jpg')] bg-cover bg-center bg-no-repeat">
+        <div className="w-screen min-h-screen bg-[url('/gradation-612x612.jpg')] bg-cover bg-center bg-no-repeat">
           {children}
         </div>
       </body>
