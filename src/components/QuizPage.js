8557bab7ed97400644f1c5473406e1aecd1cb3d2
TAKE881 <hirorogakuen@gmail.com>
2025-02-15 15:59:54 +0900

    back ground change

--- src/components/QuizPage.js
+++ src/components/QuizPage.js
@@ -4,13 +4,11 @@ import QuizPageContent from "../components/QuizPageContent";
 
 export default function QuizPage({ mode }) {
   return (
-    <div className="min-h-screen bg-cover bg-center"
-      style={{ backgroundImage: "url('/ruthvik-chandramouli-AHrljF0Zwog-unsplash.jpg')" }}>
+    <div>
       <QuizPageContent mode={mode} /> {/* ✅ `mode` を渡す */}
     </div>
   );
 }
-
 
 export async function getServerSideProps(context) {
   const { query } = context;
