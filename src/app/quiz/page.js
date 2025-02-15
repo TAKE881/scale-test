e2fa502976f418cd38d1dff9082af97b677266b3
TAKE881 <hirorogakuen@gmail.com>
2025-02-15 15:48:45 +0900

    debug

--- src/app/quiz/page.js
+++ src/app/quiz/page.js
@@ -2,8 +2,17 @@
 
 import { useSearchParams } from "next/navigation";
 import QuizPage from "@/components/QuizPage";
+import { Suspense } from "react";
 
 export default function QuizPageWrapper() {
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <QuizPageContent />
+    </Suspense>
+  );
+}
+
+function QuizPageContent() {
   const searchParams = useSearchParams();
   const mode = searchParams.get("mode") || "scaleQuiz";
 
