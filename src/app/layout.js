d97a493724f887f97cb541c7b91ac879cdd8970e
5A12 <hirorogakuen@gmail.com>
2025-02-18 15:43:51 +0900

    font change

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,15 +1,19 @@
-import { Kaisei_Tokumin } from "next/font/google";
+// import { Kaisei_Tokumin } from "next/font/google";
+import { Reggae_One } from "next/font/google";
 import "./globals.css";
 
-const kaiseiTokumin = Kaisei_Tokumin({
-  subsets: ["latin-ext"],
-  weight: ["400", "700"],
+// const kaiseiTokumin = Kaisei_Tokumin({
+const ReggaeOne = Reggae_One({
+  weight: '400',
+  subsets: ['latin'],
+  // display: 'swap',
+  // weight: ["400", "700"],
 });
 
 export default function RootLayout({ children }) {
   return (
     <html lang="ja">
-      <body className={kaiseiTokumin.className}>
+      <body className={ReggaeOne.className}>
         <div className="w-screen min-h-screen bg-[url('/gradation-612x612.jpg')] bg-cover bg-center bg-no-repeat">
           {children}
         </div>
